c7a071485c34030f74e195886d27ec17e51a3a93
jfelicio <31109635+jfelicio@users.noreply.github.com>
2026-04-14 08:20:59 +0100

    Update Footer.tsx
    Linkedin icon issue with build

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { MapPin, LinkedIn } from "lucide-react";
+import { MapPin, LinkedinIcon } from "lucide-react";
 import Link from "next/link";
 
 export default function Footer() {
